f04eeca23d0348e510e3b73849c99942840db6e9
Paul Campbell <pcampbell@kemitix.net>
2022-07-14 21:54:52 +0100

    Rename MyHomePage as WorkSpaceList

--- lib/work_notes_app.dart
+++ lib/work_notes_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'my_home_page.dart';
+import 'work_space_list.dart';
 
 class WorkNotesApp extends StatelessWidget {
   const WorkNotesApp({Key? key}) : super(key: key);
@@ -12,7 +12,7 @@ class WorkNotesApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
       ),
-      home: const MyHomePage(title: 'WorkNotes'),
+      home: const WorkSpaceList(title: 'WorkNotes'),
     );
   }
 }
--- lib/work_space_list.dart
+++ lib/work_space_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class WorkSpaceList extends StatefulWidget {
+  const WorkSpaceList({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -15,10 +15,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WorkSpaceList> createState() => _WorkSpaceListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _WorkSpaceListState extends State<WorkSpaceList> {
   int _counter = 0;
 
   void _incrementCounter() {
