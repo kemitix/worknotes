1d19906f1049c24cb8bcf88624c5c10a18bd50eb
Paul Campbell <pcampbell@kemitix.net>
2022-07-14 19:54:58 +0100

    Rename MyApp as WorknotesApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WorknotesApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class WorknotesApp extends StatelessWidget {
+  const WorknotesApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
