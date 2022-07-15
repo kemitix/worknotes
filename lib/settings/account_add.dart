c690a1fdf929ba52062537c39aa7edd75cb83acb
Paul Campbell <pcampbell@kemitix.net>
2022-07-15 20:36:09 +0100

    AccountAdd: add form fields

--- lib/settings/account_add.dart
+++ lib/settings/account_add.dart
@@ -1,11 +1,89 @@
 import 'package:flutter/material.dart';
 
-class AccountAdd extends StatelessWidget {
+class AccountAdd extends StatefulWidget {
+  const AccountAdd({Key? key}) : super(key: key);
+
+  @override
+  State<AccountAdd> createState() => _AccountAddState();
+}
+
+class _AccountAddState extends State<AccountAdd> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Account'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+              child: Text('Account Name'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              child: TextFormField(
+                decoration:
+                    const InputDecoration(hintText: 'Enter account name'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an account name';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+              child: Text('API Key'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              child: TextFormField(
+                decoration: const InputDecoration(hintText: 'Enter API Key'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an API key';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+              child: Text('Account Name'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              child: TextFormField(
+                decoration: const InputDecoration(hintText: 'Enter API Secret'),
+                obscureText: true,
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an API secret';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Process data.
+                  }
+                },
+                child: const Text('Add'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
