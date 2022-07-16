de49823a94255e449d660a134e26c5bb631aeefa
Paul Campbell <pcampbell@kemitix.net>
2022-07-16 12:36:14 +0100

    Add list of Accounts to settings/Accounts

--- lib/settings/accounts.dart
+++ lib/settings/accounts.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/accounts_model.dart';
 
 class Accounts extends StatelessWidget {
-  
   void _addAccount(BuildContext context) {
     Navigator.pushNamed(context, '/settings/accounts/add');
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Accounts'),
+      ),
+      body: Consumer<AccountsModel>(
+        builder: (context, accounts, child) => ListView.separated(
+          itemCount: accounts.accounts.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(accounts.accounts[index].name),
+              //onTap: ,
+            );
+          },
+          separatorBuilder: (a, b) => Divider(),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _addAccount(context),
