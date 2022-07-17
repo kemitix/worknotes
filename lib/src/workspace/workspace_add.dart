8173ef26772b16a6cac739aca862f47f6cf21123
Paul Campbell <pcampbell@kemitix.net>
2022-07-17 16:14:31 +0100

    WorkspaceAdd: add account selection button

--- lib/src/workspace/workspace_add.dart
+++ lib/src/workspace/workspace_add.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class WorkspaceAdd extends StatelessWidget  {
+import '../models/account.dart';
+import '../models/accounts_model.dart';
+
+class WorkspaceAdd extends StatefulWidget {
   const WorkspaceAdd({Key? key}) : super(key: key);
+
+  @override
+  State<WorkspaceAdd> createState() => _WorkspaceAddState();
+}
+
+class _WorkspaceAddState extends State<WorkspaceAdd> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String? _accountName;
+  Account? _account;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Workspace'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text('Account'),
+              Consumer<AccountsModel>(
+                builder: (context, accounts, child) {
+                  return DropdownButton(
+                    hint: const Text('Select Account'),
+                    value: _accountName,
+                    items: accounts.accounts.map((account) {
+                      return DropdownMenuItem(
+                        value: account.name,
+                        child: Text(account.name),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        _accountName = value;
+                      });
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
