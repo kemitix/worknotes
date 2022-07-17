53e6d5d39837fed35e5ab6728c55b8dd86685f7b
Paul Campbell <pcampbell@kemitix.net>
2022-07-17 18:47:19 +0100

    AccountWorkspaceList: add scrollable list of trello boards

--- lib/src/workspace/account_workspace_list.dart
+++ lib/src/workspace/account_workspace_list.dart
@@ -18,24 +18,28 @@ class AccountWorkspaceList extends StatelessWidget {
         TrelloAuthentication.of(memberId, account.key, account.secret);
     var client = dioClientFactory(authentication);
     var memberClient = client.member(memberId);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        FutureBuilder(
-          future: memberClient.getBoards(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Text('error: ${snapshot.error}');
-              }
-              var boards = snapshot.data! as List<TrelloBoard>;
-              return Text(
-                  'found ${boards.length} boards: ${boards.map((e) => e.name).join(', ')}');
-            }
-            return const Center(child: CircularProgressIndicator());
-          },
-        ),
-      ],
+    return FutureBuilder(
+      future: memberClient.getBoards(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Text('error: ${snapshot.error}');
+          }
+          var boards = snapshot.data! as List<TrelloBoard>;
+          return Expanded(
+            child: ListView.separated(
+                itemBuilder: (context, index) {
+                  var board = boards[index];
+                  return ListTile(title: Text(board.name));
+                },
+                separatorBuilder: (a, b) => const Divider(),
+                itemCount: boards.length),
+          );
+          // return Text(
+          //     'found ${boards.length} boards:\n${boards.map((e) => e.name).join('\n')}');
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
