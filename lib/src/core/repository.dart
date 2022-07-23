477951153a75cceaa5b83bd19c1104d2a5e606e2
Paul Campbell <pcampbell@kemitix.net>
2022-07-23 19:59:38 +0100

    Add Account usecase Add Account
    Reviewers: 
    
    Pull Request: https://github.com/kemitix/worknotes/pull/3

--- lib/src/core/repository.dart
+++ lib/src/core/repository.dart
@@ -1,9 +1,12 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:objectid/objectid.dart';
-import 'package:worknotes/src/core/has_id_name.dart';
+
+import 'error/failure.dart';
+import 'has_id_name.dart';
 
 abstract class Repository<T extends HasIdName> extends ChangeNotifier {
-  Future<void> add(T item);
+  Future<Either<Failure, T>> add(T item);
 
   Future<void> remove(T item);
 
