cafa4139cc4a9deb7f62aeaa8d89631839be4e23
Paul Campbell <pcampbell@kemitix.net>
2022-07-24 08:49:08 +0100

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.4

--- test/src/features/accounts/domain/usecases/add_account_test.dart
+++ test/src/features/accounts/domain/usecases/add_account_test.dart
@@ -1,14 +1,19 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:objectid/objectid.dart';
 import 'package:worknotes/src/core/error/failure.dart';
-import 'package:worknotes/src/features/accounts/data/repositories/in_memory_account_repository.dart';
 import 'package:worknotes/src/features/accounts/domain/entities/account.dart';
+import 'package:worknotes/src/features/accounts/domain/repositories/account_repository.dart';
 import 'package:worknotes/src/features/accounts/domain/usecases/add_account.dart';
 
+import 'add_account_test.mocks.dart';
+
+@GenerateMocks([AccountRepository])
 void main() {
-  InMemoryAccountRepository accountRepository = InMemoryAccountRepository();
-  AddAccount usecase = AddAccount(accountRepository);
+  MockAccountRepository mockAccountRepository = MockAccountRepository();
+  AddAccount usecase = AddAccount(mockAccountRepository);
 
   test('Should add account to repository', () async {
     //given
@@ -18,12 +23,14 @@ void main() {
         name: 'name',
         key: 'key',
         secret: 'secret');
+    when(mockAccountRepository.add(account))
+        .thenAnswer((_) async => right(account));
     //when
     final Either<Failure, void> result =
         await usecase.execute(account: account);
     //then
     expect(result, right(account));
-    expect(accountRepository.items.length, 1);
-    expect(accountRepository.items[0], account);
+    verify(mockAccountRepository.add(account));
+    verifyNoMoreInteractions(mockAccountRepository);
   });
 }
