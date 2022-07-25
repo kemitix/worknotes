1621a52d10cfbecba3d77b301f7111887ea715d7
Paul Campbell <pcampbell@kemitix.net>
2022-07-25 20:22:55 +0100

    Add SharedPreferencesAccountsLocalDataSource
    Created using spr 1.3.4

--- lib/src/features/accounts/data/repositories/local_account_repository.dart
+++ lib/src/features/accounts/data/repositories/local_account_repository.dart
@@ -1,48 +1,83 @@
-import 'package:dartz/dartz.dart';
-import 'package:objectid/src/objectid/objectid.dart';
-import 'package:worknotes/src/core/error/failure.dart';
-import 'package:worknotes/src/features/accounts/data/datasources/accounts_local_datasource.dart';
-import 'package:worknotes/src/features/accounts/domain/entities/account.dart';
-import 'package:worknotes/src/features/accounts/domain/repositories/account_repository.dart';
+import 'dart:collection';
 
+import 'package:dartz/dartz.dart';
+import 'package:objectid/objectid.dart';
+import 'package:worknotes/src/features/accounts/data/models/account_model.dart';
+
+import '../../../../core/error/duplicate_record_error.dart';
+import '../../../../core/error/failure.dart';
+import '../../../../core/error/not_found_error.dart';
+import '../../domain/entities/account.dart';
+import '../../domain/repositories/account_repository.dart';
+import '../datasources/accounts_local_datasource.dart';
+
+//TODO hook this up to the AccountsBloc
 class LocalAccountRepository extends AccountRepository {
   final AccountsLocalDataSource dataSource;
 
   LocalAccountRepository(this.dataSource);
 
-  @override
-  Future<Either<Failure, Account>> add(Account item) {
-    // TODO: implement add
-    throw UnimplementedError();
+  final List<Account> items = [];
+
+  void load() => items.addAll(dataSource.loadAccounts().map((m) => Account(
+      id: m.id, type: m.type, name: m.name, key: m.key, secret: m.secret)));
+
+  void save() => dataSource.saveAccounts(items
+      .map((e) => AccountModel(
+            id: e.id,
+            type: e.type,
+            name: e.name,
+            key: e.key,
+            secret: e.secret,
+          ))
+      .toList(growable: false));
+
+  // modifying methods:
+  // should all call notifyListeners()
+
+  Future<Either<Failure, Account>> add(Account item) async {
+    if (items.contains(item)) {
+      return Future.value(left(DuplicateError()));
+    }
+    items.add(item);
+    save();
+    notifyListeners();
+    return Future.value(right(item));
   }
 
-  @override
-  Future<Account> findById(ObjectId objectId) {
-    // TODO: implement findById
-    throw UnimplementedError();
+  Future<Either<Failure, Account>> remove(Account item) async {
+    items.remove(item);
+    save();
+    notifyListeners();
+    return Future.value(right(item));
   }
 
-  @override
-  Future<Account> findByName(String name) {
-    // TODO: implement findByName
-    throw UnimplementedError();
+  Future<void> update(int index, Account item) async {
+    items.setRange(index, index + 1, [item]);
+    save();
+    notifyListeners();
+    return Future.value(null);
   }
 
-  @override
+  // read-only methods
+
   Future<Either<Failure, List<Account>>> getAll() {
-    // TODO: implement getAll
-    throw UnimplementedError();
+    return Future.value(right(UnmodifiableListView(items)));
   }
 
-  @override
-  Future<Either<Failure, Account>> remove(Account item) {
-    // TODO: implement remove
-    throw UnimplementedError();
+  Future<Account> findById(ObjectId objectId) {
+    try {
+      return Future.value(items.firstWhere((item) => item.id == objectId));
+    } on StateError {
+      return Future.error(NotFoundError());
+    }
   }
 
-  @override
-  Future<void> update(int index, Account item) {
-    // TODO: implement update
-    throw UnimplementedError();
+  Future<Account> findByName(String name) {
+    try {
+      return Future.value(items.firstWhere((item) => item.name == name));
+    } on StateError {
+      return Future.error(NotFoundError());
+    }
   }
 }
