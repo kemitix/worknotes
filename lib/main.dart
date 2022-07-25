f134f1e8fc12494314d2c7c87423d7da338a90bf
Paul Campbell <pcampbell@kemitix.net>
2022-07-25 20:49:43 +0100

    Add and populate a Dependency Injection container
    Reviewers: 
    
    Pull Request: https://github.com/kemitix/worknotes/pull/14

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'src/injection_container.dart' as di;
 
-void main() => runApp(const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const App());
+}
